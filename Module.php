b4420bc52b9c57297de2f5c4d273922de8f50965
xjflyttp <xjflyttp@gmail.com>
2015-08-26 16:13:44 +0800

    add roles acl

--- Module.php
+++ Module.php
@@ -1,43 +1,42 @@
 <?php
-
-/**
- * rbac模块
- *
- * @example
- *
- * //config
- *
- * $config['components']['authManager'] => [
- * 'class' => 'yii\rbac\PhpManager',
- * 'itemFile' => '@common/rbac/items.php',
- * 'assignmentFile' => '@common/rbac/assignments.php',
- * 'ruleFile' => '@common/rbac/rules.php',
- * ];
- *
- * $config['modules']['rbac'] = [
- * 'class' => 'xj\rbac\Module',
- * 'userIdField' => 'uid',
- * 'userNameField' => 'username',
- * 'userTableModelName' => '\common\models\Members',
- * 'allowedIPs' => ['127.0.0.1', '::1'],
- * 'layout' => 'main-parent',
- * ];
- */
-
 namespace xj\rbac;
 
 use Yii;
+use yii\base\InvalidConfigException;
+use yii\di\Instance;
 use yii\web\ForbiddenHttpException;
+use yii\web\User;
 
+/**
+ * RbacModule
+ * @author xjflyttp <xjflyttp@gmail.com>
+ */
 class Module extends \yii\base\Module
 {
 
     public $controllerNamespace = 'xj\rbac\controllers';
-    public $userIdField = 'id';
-    public $userNameField = 'name';
-    public $userTableModelName = '\common\models\Members';
-    public $allowedIPs = ['127.0.0.1', '::1'];
+    public $userIdField;
+    public $userNameField;
+    public $userTableModelName;
+    public $allowedIPs;
+    public $roles;
     public $layout = 'main';
+    public $user = 'user';
+
+    public function init()
+    {
+        parent::init();
+        if (null === $this->userIdField) {
+            throw new InvalidConfigException('userIdField is null');
+        } elseif (null === $this->userNameField) {
+            throw new InvalidConfigException('userNameField is null');
+        } elseif (null === $this->userTableModelName) {
+            throw new InvalidConfigException('userTableModelName is null');
+        } elseif (null === $this->allowedIPs && null === $this->roles) {
+            throw new InvalidConfigException('allowedIPs & roles both null');
+        }
+        $this->user = Instance::ensure($this->user, User::className());
+    }
 
     /**
      * @inheritdoc
@@ -48,7 +47,7 @@ class Module extends \yii\base\Module
             return false;
         }
 
-        if (Yii::$app instanceof \yii\web\Application && !$this->checkAccess()) {
+        if (!$this->checkAccess()) {
             throw new ForbiddenHttpException('You are not allowed to access this page.');
         }
 
@@ -60,14 +59,49 @@ class Module extends \yii\base\Module
      */
     protected function checkAccess()
     {
+
         $ip = Yii::$app->getRequest()->getUserIP();
+        if ($this->checkIps($ip) && $this->checkRoles($this->user)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param string $ip
+     * @return bool
+     */
+    protected function checkIps($ip)
+    {
         foreach ($this->allowedIPs as $filter) {
             if ($filter === '*' || $filter === $ip || (($pos = strpos($filter, '*')) !== false && !strncmp($ip, $filter, $pos))) {
                 return true;
             }
         }
         Yii::warning('Access to Rbac is denied due to IP address restriction. The requested IP is ' . $ip, __METHOD__);
+        return false;
+    }
 
+    /**
+     * @param User $user
+     * @return bool
+     */
+    protected function checkRoles($user)
+    {
+        foreach ($this->roles as $role) {
+            if ($role === '?') {
+                if ($user->getIsGuest()) {
+                    return true;
+                }
+            } elseif ($role === '@') {
+                if (!$user->getIsGuest()) {
+                    return true;
+                }
+            } elseif ($user->can($role)) {
+                return true;
+            }
+        }
+        Yii::warning('Access to Rbac is denied');
         return false;
     }
 }
--- README.md
+++ README.md
@@ -1,35 +1,69 @@
-yii2-rbac-module
-=======================
+# yii2-rbac-module
 
 
-composer.json
-===================
-````````
-"require": {
-    "yii2-rbac-module": "*"
-},
-````````
+## composer.json
+```json
+"xj/yii2-rbac-module": "~1.0.0",
+```
 
 ### config
-`````````````````````php
- $config['components']['authManager'] => [
-    'class' => 'yii\rbac\PhpManager',
-    'itemFile' => '@common/rbac/items.php',
-    'assignmentFile' => '@common/rbac/assignments.php',
-    'ruleFile' => '@common/rbac/rules.php',
-];
+---
+```php
+'components' => [
+    'authManager' => [
+        'class' => 'yii\rbac\PhpManager',
+        'itemFile' => '@common/rbac/items.php',
+        'assignmentFile' => '@common/rbac/assignments.php',
+        'ruleFile' => '@common/rbac/rules.php',
+    ],
+],
+```
 
-$config['modules']['rbac'] = [
-    'class' => 'xj\rbac\Module',
-    'userIdField' => 'uid',
-    'userNameField' => 'username',
-    'userTableModelName' => '\common\models\Members',
-    'allowedIPs' => ['127.0.0.1', '::1'], //dump from yii\gii
-    'layout' => 'main',  //main-parent=use app main | main=rbac layout
-];
-`````````````````````
-
+#### IP & Roles
+---
+```php
+'modules' => [
+    'rbac' => [
+        'class' => 'xj\rbac\Module',
+        'userIdField' => 'id',
+        'userNameField' => 'username',
+        'userTableModelName' => '\common\models\Admin',
+        'allowedIPs' => ['127.0.0.1', '::1'],
+        'roles' => ['admin'],
+    ]
+],
+```
+#### IP Only
+---
+```php
+'modules' => [
+    'rbac' => [
+        'class' => 'xj\rbac\Module',
+        'userIdField' => 'id',
+        'userNameField' => 'username',
+        'userTableModelName' => '\common\models\Admin',
+        'layout' => 'main', //optional
+        'allowedIPs' => ['127.0.0.1', '::1'],
+        'roles' => ['*'],
+    ]
+],
+```
+#### Role Only
+---
+```php
+'modules' => [
+    'rbac' => [
+        'class' => 'xj\rbac\Module',
+        'userIdField' => 'id',
+        'userNameField' => 'username',
+        'userTableModelName' => '\common\models\Admin',
+        'allowedIPs' => ['*'],
+        'roles' => ['admin'],
+    ]
+],
+```
 ### access
-```````````
+---
+```
 http://project/index.php?r=rbac
-````````````
+```
